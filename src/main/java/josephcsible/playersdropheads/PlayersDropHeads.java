deee1d7b1abf90f36b1d06170d7fb17ae009594c
Joseph C. Sible <josephcsible@users.noreply.github.com>
2018-02-06 20:35:34 -0500

    Name the mod

--- src/main/java/josephcsible/playersdropheads/PlayersDropHeads.java
+++ src/main/java/josephcsible/playersdropheads/PlayersDropHeads.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package josephcsible.playersdropheads;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
@@ -7,12 +7,12 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = PlayersDropHeads.MODID, name = PlayersDropHeads.NAME, version = PlayersDropHeads.VERSION)
+public class PlayersDropHeads
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "playersdropheads";
+    public static final String NAME = "PlayersDropHeads";
+    public static final String VERSION = "1.0.0";
 
     private static Logger logger;
 
